f09b27140cba5ab34da721839eee41abe3f8ddba
queviva <jabraham.engineer@gmail.com>
2022-02-22 22:53:12 -0800

    this is a MUCH smaller and slicker calculation, comparing area of polygon to area of bounding circle

--- clockwizz.js
+++ clockwizz.js
@@ -6,19 +6,18 @@
 
     O=Object,
     
-    P=O.assign({
+    P=Object.assign({
         
         selector        : 'wizz',
         preventDefault  : true
         
-    },JSON.parse(O.values(D)[0]||'{}')),
+    },JSON.parse(Object.values(D)[0]||'{}')),
     
     R=P.selector,
     
     T='touches',
     
     M=Math,
-    
     B=M.abs,
     
     PI80=180/M.PI
@@ -29,60 +28,40 @@
     
     N,V,J,
     
-    F=O.assign({},P,JSON.parse(N.dataset[R]||'{}')),
+    F=Object.assign({},P,JSON.parse(N.dataset[R]||'{}')),
     
     [C,A,D]='123'.split('').map((v,i)=>
         new Array(6-i).fill(0).map(k=>i>0?k:{x:0,y:0})
     ),
     
     W=(e,m)=>{
-        
+    
         C.shift();
-
-        C.push({x:m.pageX,y:m.pageY});
-        
-        A = A.map((a,i) =>
-            Math.atan2(
-               C[i+1].y - C[0].y,
-               C[i+1].x - C[0].x
-            ) * PI80
+    
+        C.push({ x: m.pageX, y: m.pageY });
+    
+        let mag = M.max(
+            M.abs(C[0].x-C.at(-1).x),
+            M.abs(C[0].y-C.at(-1).y)
         );
         
-        D = D.map((d,i) => {
-            let tmp = A[i + 1] - A[i]
-            return tmp > 180 ? tmp - 360 : tmp < -180 ? tmp + 360 : tmp;
-        });
-        
-        let absD = D.map(d=>
-            Math.abs(d) > 90 ||
-            Math.abs(d) <  2 ?
-            0.000069 : Math.abs(d)
+        let area = C.reduce((p, c, i) => p +
+                (C[(i + 1) % 6].x + c.x) *
+                (C[(i + 1) % 6].y - c.y), 0
         );
         
-        let sig = M.sign(D.at(-1));
+        let circ = 0.785 * mag * mag;
         
-        let dir = 0;
+        let dir =
         
-        if (
-            sig !== 0 &&
-            D.every(d=>M.sign(d) === sig) &&
-            absD.every(a=>
-                a !==0 &&
-                a < 90 &&
-                a >  2
-            )
-        ) {
-            dir = sig;
-        }
+            B(area)/circ > 0.2 && mag > 5
         
-        N.dispatchEvent(new CustomEvent(
-            'clock-'+F.selector,{
+            ? M.sign(area) : 0;
+        
+        N.dispatchEvent(new CustomEvent('clock-'+R, {
             detail: {
                 dir: dir,
-                mag: M.max(
-                  M.abs(C[0].x-C.at(-1).x),
-                  M.abs(C[0].y-C.at(-1).y)
-                ),
+                mag: mag,
                 ref: e
             }
         }));
@@ -101,7 +80,7 @@
 
 )=>[N,window].forEach((J,j)=>
     
-    O.keys(Z).forEach((z,i)=>J.addEventListener(
+    Object.keys(Z).forEach((z,i)=>J.addEventListener(
         [['mousedown','touchstart'],
         ['mouseup','touchend']][j][i],
         e=>window[(j===0?'add':'remove')+'EventListener']
--- wizzmicro.js
+++ wizzmicro.js
@@ -34,60 +34,33 @@
         new Array(6-i).fill(0).map(k=>i>0?k:{x:0,y:0})
     ),
     
+    Xs = new Array(6).fill(0),
+    Ys = new Array(6).fill(0),
+    
     W=(e,m)=>{
-        
+    
         C.shift();
-
-        C.push({x:m.pageX,y:m.pageY});
-        
-        /*
-        let dir = Math.sign(C.reduce((p,c,i) => p+
-           (C[(i+1)%6].x - c.x)*
-           (C[(i+1)%6].y + c.y),0
-        ));
-        */
-        
-        A = A.map((a,i) =>
-            Math.atan2(
-               C[i+1].y - C[0].y,
-               C[i+1].x - C[0].x
-            ) * PI80
+    
+        C.push({ x: m.pageX, y: m.pageY });
+    
+        let mag = M.max(
+            M.abs(C[0].x-C.at(-1).x),
+            M.abs(C[0].y-C.at(-1).y)
         );
         
-        D = D.map((d,i) => {
-            let tmp = A[i + 1] - A[i]
-            return tmp > 180 ? tmp - 360 : tmp < -180 ? tmp + 360 : tmp;
-        });
-        
-        let absD = D.map(d=>
-            Math.abs(d) > 90 ||
-            Math.abs(d) <  2 ?
-            0.000069 : Math.abs(d)
+        let area = C.reduce((p, c, i) => p +
+                (C[(i + 1) % 6].x + c.x) *
+                (C[(i + 1) % 6].y - c.y), 0
         );
         
-        let sig = M.sign(D.at(-1));
+        let circ = 0.785 * mag * mag;
         
-        let dir = 0;
-        
-        if (
-            sig !== 0 &&
-            D.every(d=>M.sign(d) === sig) &&
-            absD.every(a=>
-                a !==0 &&
-                a < 90 &&
-                a >  2
-            )
-        ) {
-            dir = sig;
-        }
+        let dir = B(area)/circ > 0.2 ? M.sign(area) : 0;
         
         N.dispatchEvent(new CustomEvent('clock-'+R, {
             detail: {
                 dir: dir,
-                mag: M.max(
-                  M.abs(C[0].x-C.at(-1).x),
-                  M.abs(C[0].y-C.at(-1).y)
-                ),
+                mag: mag,
                 ref: e
             }
         }));
@@ -98,89 +71,7 @@
         }
         
     },
-    
-    xxW=(e,m)=>{
 
-        C.shift();
-
-        C.push({x:m.pageX,y:m.pageY});
-
-        C.forEach((c,i)=>{
-            
-            A[i - 1] = (M.atan2(
-
-                c.y - C[0].y,
-                c.x - C[0].x
-
-            ) * PI80);
-            
-        });
-        
-        A.forEach((a,i)=>{
-
-            let tmp = a - A[i - 1];
-
-            tmp =
-                tmp >  180 ? tmp - 360 :
-                tmp < -180 ? tmp + 360 :
-                tmp;
-
-            D[i - 1] = tmp;
-
-        })
-
-        let absDeltas = D.map((v, i) =>
-
-            M.abs(v) > 90 ||
-            M.abs(v) < 2 ?
-            69e-9 : M.abs(v)
-
-        );
-        
-        let signFinalDelta = M.sign(D.at(-1));
-
-        let dir = 0;
-
-        if (
-
-            signFinalDelta !== 0
-
-            &&
-
-            D.every(v =>
-
-                M.sign(v) === signFinalDelta
-
-            )
-
-            &&
-
-            absDeltas.every(v => v > 2)
-
-        ) {
-
-            dir = signFinalDelta;
-
-        }
-
-        N.dispatchEvent(new CustomEvent('clock-'+R, {
-            detail: {
-                dir: dir,
-                mag: M.max(
-                  M.abs(C[0].x-C.at(-1).x),
-                  M.abs(C[0].y-C.at(-1).y)
-                ),
-                ref: e
-            }
-        }));
-
-        if(F.preventDefault){
-            e.preventDefault();
-            e.stopPropagation();
-        }
-        
-    },
-    
     Z={
         mousemove:e=>W(e,e),
         touchmove:e=>W(e,e[T][0])
