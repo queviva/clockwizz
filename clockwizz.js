10d8f5bd1e07e3fadcce46d0e9a99f94af8bede8
queviva <jabraham.engineer@gmail.com>
2022-02-22 23:40:47 -0800

    some cleaning up

--- clockwizz.js
+++ clockwizz.js
@@ -3,10 +3,8 @@
     O=Object,
     
     P=Object.assign({
-        
         selector        : 'wizz',
-        preventDefault  : true
-        
+        preventDefault  :  true
     },JSON.parse(Object.values(D)[0]||'{}')),
     
     R=P.selector,
@@ -15,11 +13,9 @@
     
     M=Math,
     
-    B=M.abs,
+    B=M.abs
     
-    PI80=180/M.PI
-    
-) => document.querySelectorAll(`[data-${R}]:not(script)`
+)=>document.querySelectorAll(`[data-${R}]:not(script)`
 
 ).forEach((
     
@@ -31,31 +27,27 @@
     
     W=(e,m)=>{
     
-        C.shift();
-    
-        C.push({ x: m.pageX, y: m.pageY });
+        C.shift(), C.push({x:m.pageX,y:m.pageY});
     
         let mag = M.max(
-            M.abs(C[0].x-C.at(-1).x),
-            M.abs(C[0].y-C.at(-1).y)
-        );
+            B(C[0].x-C.at(-1).x),
+            B(C[0].y-C.at(-1).y)
+        ),
         
-        let area = C.reduce((p, c, i) => p +
-                (C[(i + 1) % 6].x + c.x) *
-                (C[(i + 1) % 6].y - c.y), 0
-        );
+        area = C.reduce((p, c, i) => p +
+            (C[(i+1)%6].x + c.x) *
+            (C[(i+1)%6].y - c.y),0
+        ),
         
-        let dir =
-        
-            B(area)/(0.785 * mag * mag) > 0.2 && mag > 5
-        
+        dir =
+            B(area)/(0.785*mag*mag)>0.2&&mag>5
             ? M.sign(area) : 0;
         
         N.dispatchEvent(new CustomEvent('clock-'+R, {
             detail:{dir:dir,mag:mag,ref:e}
         }));
-
-        if(F.preventDefault){
+        
+        if (F.preventDefault){
             e.preventDefault();
             e.stopPropagation();
         }
