2679e00637470423757e5c44d60e6f6ccbf5d5bb
queviva <jabraham.engineer@gmail.com>
2022-03-01 17:32:27 -0800

    better arrangement, added minimum option and corrected individual wizzer event names

--- clockwizz.js
+++ clockwizz.js
@@ -1,79 +1,69 @@
-((D)=>document.addEventListener('DOMContentLoaded',(E,
+((D)=>document.addEventListener('DOMContentLoaded',(
 
-    O=Object,
-    
-    P=Object.assign({
-        selector        : 'wizz',
-        preventDefault  :  true
-    },
-    JSON.parse(Object.values(D)[0]||'{}')),
-    
-    R=P.selector,
-    
+    E,O=Object,J=JSON.parse,M=Math,B=M.abs,
+
     T='touches',
     
-    M=Math,
-    
-    B=M.abs
-    
-)=>document.querySelectorAll(`[data-${R}]:not(script)`).forEach((
-    
+    P=O.assign({
+
+        selector        : 'wizz',
+        minimum         : 5,
+        preventDefault  : true
+
+    },J(O.values(D)[0]||'{}')),
+
+    X=P.selector
+
+)=>document.querySelectorAll(`[data-${X}]:not(script)`).forEach((
+
     W,I,N,
-    
-    F=Object.assign({},P,JSON.parse(W.dataset[R]||'{}')),
+
+    F=O.assign({},P,J(W.dataset[X]||'{}')),
     
     C=Array.from({length:6},c=>({x:0,y:0})),
-    
-    L=(e,g)=>{
-    
-        C.shift(), C.push({x:g.pageX,y:g.pageY});
-    
-        let m=M.max(
-            B(C[0].x-C.at(-1).x),
-            B(C[0].y-C.at(-1).y)
-        ),
-        
-        a=C.reduce((p,c,i)=>p+
-            (C[(i+1)%6].x + c.x) *
-            (C[(i+1)%6].y - c.y),0
-        ),
-        
-        d=B(a)/m/m>0.175 // % roundness
-        
-        &&m>5 // minimum radius
-        
-        &&C.every(c=>c.x!==0) // prevent jumps
-        
-        ?M.sign(a):0;
-        
-        W.dispatchEvent(new CustomEvent('clock-'+R, {
-            detail:{dir:d,mag:m,ref:e}
-        }));
-        
-        F.preventDefault&&(
-        e.preventDefault(),
-        e.stopPropagation());
-        
-    },
 
-    Z={mouse:e=>L(e,e),touch:e=>L(e,e[T][0])}
-    
-)=>Object.keys(Z).forEach((
-    
+    Z={mouse:e=>L(e,e),touch:e=>L(e,e[T][0])},
+
+    L=(e,g,m,a=C.shift()+C.push({x:g.pageX,y:g.pageY}))=>
+    W.dispatchEvent(new CustomEvent('clock-'+F.selector,{
+        detail:{
+
+            ref:F.preventDefault&&(
+                e.preventDefault(),
+                e.stopPropagation())||e,
+
+            mag:m=M.max(
+                B(C[0].x-C[5].x),
+                B(C[0].y-C[5].y)
+            ),
+
+            dir:m>F.minimum&&C.every(c=>!!c.x)&&
+
+                B(a=C.reduce((p,c,i,_,j=(i+1)%6)=>p+
+                    (C[j].x+c.x)*(C[j].y-c.y),0
+                ))/m/m>0.18?M.sign(a):0
+
+        }
+    }))
+
+)=>O.keys(Z).forEach((
+
     z,i,p,a=[z+'move',Z[z],{passive:false}]
-    
+
 )=>W.addEventListener(
-    
+
     z+['down','start'][i],
-    
+
     ()=>[a,[z+['up','end'][i],()=>(
-        
+
         C=C.map(c=>({x:0,y:0})),
-        
+
         window.removeEventListener(...a)),
-        
+
         {once:true}]
-        
-    ].forEach(e=>window.addEventListener(...e))
-        
-)))))(document.currentScript.dataset);
+
+    ].forEach(p=>window.addEventListener(...p))
+
+))
+
+)))(document.currentScript.dataset)
